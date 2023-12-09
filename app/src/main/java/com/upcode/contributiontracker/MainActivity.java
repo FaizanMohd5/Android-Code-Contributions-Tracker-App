4bc9d229eb760165a3e2de147893000137b65525
Faizan Mohammed <faizanmohammed416@gmail.com>
2023-12-10 00:56:17 +0530

    First page created.

---
+++ app/src/main/java/com/upcode/contributiontracker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.upcode.contributiontracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
